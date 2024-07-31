3b859c32b8b72a7a13490869a5d09533483eb18a
shakilahmad777 <shakilahmadpip3@gmail.com>
2024-07-31 21:02:02 +0600

    this added by 2files letContant or scope js

---
+++ jses6/letConstant.js
@@ -0,0 +1,31 @@
+// lets goooo!!
+
+// variable is very simple patro jar modhe barbar leclar kora jay and barbar assaign kora jay and unsecurity patro jake barbar declar and assaign kora jay //
+
+var name = 'shakil'; // Allow
+
+    name = "ahmad"  // Allow
+
+var name = 'shakil ahmad'   // Allow
+
+console.log(name)
+
+// let is a one daimontion patro jake only on by akbar declar kora jay and barbar assaign kora jay but same name diye second bar ar decler kora jay nah //
+
+let name1 = 'imran';    // Allow
+
+    name1 = 'bappy';    // Allow
+
+//let name1 = 'imran bappy'    // NOT Allow
+
+console.log(name1)
+
+// constant (const) is a very care fully patro jake only on declar and assaign kora jay second declaration you think or mind in error //
+
+const name2 = 'i love js';  // Allow
+
+//    name2 = 'i love js' // NOT Allow
+// const name2 = 'i love js allways'   // NOT Allow
+
+console.log(name2)
+
---
+++ jses6/scope.js
@@ -0,0 +1,23 @@
+// global scope
+// local scope
+// block scope
+
+// this is global scope
+
+let name = 'shakil';
+let isMale = true;
+
+console.log(name, isMale);
+
+//  this is block scope
+
+{
+    let roll = 20110;
+
+    console.log(roll)
+}
+// console.log(roll) // this is not good! this reject.
+
+
+// this is local scope
+
